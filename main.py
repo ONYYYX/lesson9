0ed08176081575f7dd5f023d1b9ddaeba2d171e6
ONYYYX <onyxservers@yandex.ru>
2020-03-11 10:03:21 +0400

    Deployment of modules

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from db import db_session
-from data.User import User
+from data.Jobs import Jobs
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'my_skey_lol_lmao'
@@ -8,18 +8,14 @@ db_session.global_init('db/blogs.sqlite')
 
 
 def main():
-    # для запуска приложения, меняйте почту, она должна быть уникальной
-    user = User()
-    user.surname = 'iuawdvnawjd'
-    user.name = 'awdvawidmwkamdnkv'
-    user.age = 3
-    user.position = 'child'
-    user.speciality = 'child'
-    user.address = 'module_123'
-    user.email = 'scott_is_father_ahahah@mars.org'
-    user.hashed_password = '1234124'
+    job = Jobs()
+    job.leader_id = 1
+    job.job = 'deployment of residential modules 1 and 2'
+    job.work_size = 15
+    job.collaborators = '2, 3'
+    job.is_finished = False
     session = db_session.create_session()
-    session.add(user)
+    session.add(job)
     session.commit()
 
     app.run()
